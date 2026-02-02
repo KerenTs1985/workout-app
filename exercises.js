d6aee518b1e03ba4d976e700b6a16a4b80222858
KerenTs1985 <tsrork@gmail.com>
2026-02-02 14:12:00 +0200

    Add files via upload

---
+++ exercises.js
@@ -0,0 +1,5 @@
+[
+  {"id": 1, "name": "Dumbbell Bench Press", "category": "חזה"},
+  {"id": 2, "name": "Bent Over DB Row", "category": "גב"},
+  {"id": 3, "name": "Shoulder Press", "category": "כתפיים"}
+]
